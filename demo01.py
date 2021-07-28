ffd59c99a139630c288c560a52de6e21b64dd9a2
GReeeeNc <1013749838@qq.com>
2021-07-28 14:48:41 +0800

    第一次向githob提交代码

---
+++ demo01.py
@@ -0,0 +1,21 @@
+"""
+print("hellow,python")  #字符串
+print(123)              #整数
+print(1.11)             #小数
+print(True)             #布尔值
+print(())               #元组
+print([])               #数组
+print({})               #字典
+"""
+
+
+# a = float(input("请输入第一个数字"))
+# b = float(input("请输入第二个数字"))
+# print(a+b)
+
+# a='aaa'
+# print(len(a))
+
+a="123"
+b="点击奥斯"
+print(len(a)+len(b))
